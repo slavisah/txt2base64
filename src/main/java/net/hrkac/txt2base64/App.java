51e036a0516790d8de203299df078f9c000d5ce8
slavisah <shrkac@gmail.com>
2015-03-12 14:22:28 +0100

    better formatting

--- src/main/java/net/hrkac/txt2base64/App.java
+++ src/main/java/net/hrkac/txt2base64/App.java
@@ -9,26 +9,26 @@ import java.io.OutputStream;
 import sun.misc.BASE64Encoder;
 
 public class App {
-	
+
     private static final String COMMAND_SYNTAX = "java -jar txt2base64.jar <input text file>";
     private static final String EXTENSION = ".base64";
-	
-    public static void main( String[] args ) {
-    	if (args.length == 0) {
-    		System.err.println("Input txt file is missing! Aborting...\nCommand syntax: \n" + COMMAND_SYNTAX);
-    		System.exit(0);
-    	}
-    	OutputStream output = null;
-		try {
-			InputStream input = new FileInputStream(args[0]);
-			BASE64Encoder encoder = new BASE64Encoder();
-			output = new FileOutputStream(args[0] + EXTENSION);
-			encoder.encodeBuffer(input, output);
-			output.flush();
-			output.close();
-			System.out.println("Base64 file successfully created as " + args[0] + EXTENSION);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Input txt file is missing! Aborting...\nCommand syntax: \n" + COMMAND_SYNTAX);
+            System.exit(0);
+        }
+        OutputStream output = null;
+        try {
+            InputStream input = new FileInputStream(args[0]);
+            BASE64Encoder encoder = new BASE64Encoder();
+            output = new FileOutputStream(args[0] + EXTENSION);
+            encoder.encodeBuffer(input, output);
+            output.flush();
+            output.close();
+            System.out.println("Base64 file successfully created as " + args[0] + EXTENSION);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/net/hrkac/txt2base64/AppTest.java
+++ src/test/java/net/hrkac/txt2base64/AppTest.java
@@ -4,16 +4,15 @@ import net.hrkac.txt2base64.App;
 
 import org.junit.Test;
 
-
 public class AppTest {
-	
-	@Test
+
+    @Test
     public void testApp() {
-    	App.main(new String[]{"src/test/resources/input.txt"});
+        App.main(new String[] { "src/test/resources/input.txt" });
     }
-	
-	@Test
-	public void testApp_NoArgument() {
-		App.main(new String[]{});
-	}
+
+    @Test
+    public void testApp_NoArgument() {
+        App.main(new String[] {});
+    }
 }
